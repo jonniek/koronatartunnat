f27db2c23f30474600f551603d3e4541c4d98329
Jonniek <kayhty@windowslive.com>
2020-04-08 13:14:14 +0300

    fix axios data extraction

--- prebuild/index.js
+++ prebuild/index.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const axios = require('axios');
 
 const init = async () => {
-	const json = await axios("https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData/v2")
-	fs.writeFileSync('build/apidata.json', JSON.stringify(json))
+	const { data } = await axios("https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData/v2")
+	fs.writeFileSync('build/apidata.json', JSON.stringify(data))
 }
 
 init().catch(e => {
