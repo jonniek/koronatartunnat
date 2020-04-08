4517a22f7fce9467995a5a5146d87ad6df50db09
Jonniek <kayhty@windowslive.com>
2020-04-08 11:57:33 +0300

    change node-fetch to axios since it throws on status >299

--- prebuild/index.js
+++ prebuild/index.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 const init = async () => {
-	const response = await fetch("https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData/v2")
-	const json = await response.json()
+	const json = await axios("https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData/v2")
 	fs.writeFileSync('build/apidata.json', JSON.stringify(json))
 }
 
 init().catch(e => {
-	console.error(e)
+	console.error("PREBUILD script failed " + e.message)
 	process.exit(1)
 })
